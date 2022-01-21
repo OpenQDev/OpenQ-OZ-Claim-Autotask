6012260d37a37d404957fd41421c5ea805f9abf1
Flaco <andrew@openq.dev>
2022-01-20 21:05:42 -0600

    working with logs

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const { DefenderRelayProvider, DefenderRelaySigner } = require('defender-relay-c
 const { ethers } = require('ethers');
 
 const OPENQ_ABI = [{"inputs":[{"internalType":"string","name":"_id","type":"string"},{"internalType":"address","name":"_payoutAddress","type":"address"}],"name":"claimBounty","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-const OPENQ_ADDRESS = `0x5fB848E68Ed73D38195c9970Ac19a627f2fbFaf1`;
+const OPENQ_ADDRESS = `0xD318206C31159A1dB5c2e27E7f41DD5AD95D7C19`;
 
 // Entrypoint for the Autotask
 exports.handler = async function(event) {
   const { issueId, payoutAddress } = event.request.body;
+  console.log(`Attempting to claim issue ${issueId} to payoutAddress ${payoutAddress}`)
   const provider = new DefenderRelayProvider(event);
   const signer = new DefenderRelaySigner(event, provider, { speed: 'fastest' });
   const openQ = new ethers.Contract(OPENQ_ADDRESS, OPENQ_ABI, signer);
-  const txn = await openQ.claimBounty(issueId, payoutAddress);
-  console.log(`Called execute in ${txn.hash}`);
+  const options = { gasLimit: 3000000 };
+  const txn = await openQ.claimBounty(issueId, payoutAddress, options);
+  console.log(`Called claimBounty in ${txn.hash}`);
   return { txn: txn.hash };
 }
 
