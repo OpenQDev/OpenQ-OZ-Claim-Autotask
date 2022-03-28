9935a2e3a8e51afc323ca426daab437b6e99aa3f
FlacoJones <andrew@openq.dev>
2022-03-28 12:24:14 -0500

    adds RegEx for extracting all styles and cases of issue numbers

--- __tests__/closerCommentRegex.test.js
+++ __tests__/closerCommentRegex.test.js
@@ -4,28 +4,23 @@ describe('closerCommentRegex', () => {
 	// Well-formed strings
 	let simpleCloses = 'Closes #14';
 	let simpleFixes = 'fixes #1';
-	let closesOrg = 'Closes OpenQDev/OpenQ-Frontend#143';
+	let closesOrg = 'Closes OpenQDev/OpenQ-TestRepo#143';
+	let multipleCloses = 'this Closes #14 and later also closes OpenQDev/OpenQ-TestRepo#543 and other things';
 
 	// Malformed/Non-existent closer comment
 	let noCloser = 'I contain no closer';
 	let malformed = 'Closer #d';
 
 	it('Returns true and proper number for well-formed closer comments', async () => {
-		expect(closerCommentRegex(simpleCloses).containsCloser).toEqual(true);
-		expect(closerCommentRegex(simpleCloses).issueNumber).toEqual(14);
-
-		expect(closerCommentRegex(closesOrg).containsCloser).toEqual(true);
-		expect(closerCommentRegex(closesOrg).issueNumber).toEqual(143);
-
-		expect(closerCommentRegex(simpleFixes).containsCloser).toEqual(true);
-		expect(closerCommentRegex(simpleFixes).issueNumber).toEqual(1);
+		expect(closerCommentRegex(simpleCloses, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([14]);
+		expect(closerCommentRegex(simpleFixes, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([1]);
+		expect(closerCommentRegex(closesOrg, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([143]);
+		expect(closerCommentRegex(noCloser, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([]);
+		expect(closerCommentRegex(multipleCloses, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([14, 543]);
 	});
 
 	it('Returns false and undefined for no closer or malformed closer', async () => {
-		expect(closerCommentRegex(noCloser).containsCloser).toEqual(false);
-		expect(closerCommentRegex(noCloser).issueNumber).toEqual(NaN);
-
-		expect(closerCommentRegex(malformed).containsCloser).toEqual(false);
-		expect(closerCommentRegex(malformed).issueNumber).toEqual(NaN);
+		expect(closerCommentRegex(noCloser, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([]);
+		expect(closerCommentRegex(malformed, 'OpenQDev', 'OpenQ-TestRepo')).toEqual([]);
 	});
 });
