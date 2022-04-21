3b09be9f39daa653c249d639840ee7d683f35f61
FlacoJones <andrew@openq.dev>
2022-04-21 12:45:45 -0500

    uncomments tests

--- __tests__/checkWithdrawalEligibility.test.js
+++ __tests__/checkWithdrawalEligibility.test.js
@@ -91,15 +91,15 @@ describe('checkWithdrawalEligibility', () => {
 				await expect(checkWithdrawalEligibility(issueUrl, oauthToken)).resolves.toEqual({ 'canWithdraw': true, type: 'SUCCESS', issueId: 'I_kwDOGWnnz85GjwA1', errorMessage: null });
 			});
 
-			// it('should resolve to with canWithdraw: true if eligible pull request is connected in COMMENTS', async () => {
-			// 	const issuePrData = { data: { viewer: { login: "FlacoJones" }, resource: { id: "I_kwDOGWnnz85GjwA1", number: 136, repository: { owner: { login: "OpenQDev" } }, timelineItems: { edges: [{ node: { createdAt: "2022-03-28T17:47:26Z", source: { __typename: "PullRequest", bodyText: "This Closes #136 and also unrelated thing of Fixes #137", mergedAt: "2022-03-28T17:57:44Z", createdAt: "2022-03-28T17:47:26Z", userContentEdits: { edges: [] }, comments: { edges: [] }, merged: true, url: "https://github.com/OpenQDev/OpenQ-TestRepo/pull/138", author: { login: "FlacoJones" }, baseRepository: { name: "OpenQ-TestRepo", owner: { login: "OpenQDev" } } } } }] } } } };
+			it('should resolve to with canWithdraw: true if eligible pull request is connected in COMMENTS', async () => {
+				const issuePrData = { data: { viewer: { login: "FlacoJones" }, resource: { id: "I_kwDOGWnnz85GjwA1", number: 136, repository: { owner: { login: "OpenQDev" } }, timelineItems: { edges: [{ node: { createdAt: "2022-03-28T17:47:26Z", source: { __typename: "PullRequest", bodyText: "This Closes #136 and also unrelated thing of Fixes #137", mergedAt: "2022-03-28T17:57:44Z", createdAt: "2022-03-28T17:47:26Z", userContentEdits: { edges: [] }, comments: { edges: [] }, merged: true, url: "https://github.com/OpenQDev/OpenQ-TestRepo/pull/138", author: { login: "FlacoJones" }, baseRepository: { name: "OpenQ-TestRepo", owner: { login: "OpenQDev" } } } } }] } } } };
 
-			// 	mock
-			// 		.onPost('https://api.github.com/graphql')
-			// 		.replyOnce(200, issuePrData);
+				mock
+					.onPost('https://api.github.com/graphql')
+					.replyOnce(200, issuePrData);
 
-			// 	await expect(checkWithdrawalEligibility(issueUrl, oauthToken)).resolves.toEqual({ 'canWithdraw': true, type: 'SUCCESS', issueId: 'I_kwDOGWnnz85GjwA1', errorMessage: null });
-			// });
+				await expect(checkWithdrawalEligibility(issueUrl, oauthToken)).resolves.toEqual({ 'canWithdraw': true, type: 'SUCCESS', issueId: 'I_kwDOGWnnz85GjwA1', errorMessage: null });
+			});
 		});
 	});
 });
